aa9336e2ed1a1b20074120d91e09b01863e4e7dc
MShArAFidDiNM <sharafiddin2007@outlook.com>
2023-03-28 16:22:51 +0500

    Nested loop

---
+++ NESTED loop_1/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace find_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Row : ");
+            int row, col;
+            row = int.Parse(Console.ReadLine());
+            Console.Write("Col : ");
+            col = int.Parse(Console.ReadLine());
+            int h, m;
+            h = (row / 2) ;
+            m = (col / 2) ;
+            for (int i = 0; i < row; i++)
+            {
+                for(int j = 0; j < col; j++)
+                {
+                    if ((!(i==j &&i + j== col-1))&&(i == h || j == m ))
+                    {
+                    Console.Write(" * ");
+                    }
+                    else
+                    {
+                       Console.Write("   ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
